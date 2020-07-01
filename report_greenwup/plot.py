9d1d6107166ec193ad5179c01a32f6c8ee29cada
Leonardo Emili <leonardo.emili3@gmail.com>
2020-07-01 17:59:52 +0200

    Update charts adding measurement units

--- report_greenwup/plot.py
+++ report_greenwup/plot.py
@@ -11,12 +11,12 @@ from matplotlib.ticker import PercentFormatter
 # 3 - overhead
 m_type = 0
 
-# Select if plotting simulation with interest dissemination phase
+# Select if plotting simulation with interest dissemination phase (energy only - m_type should be 0)
 interest_active = True
 
 # Names and labels
 types = ["energy", "latency", "pdr", "overhead"]
-labels = ["Energy spent by the network [J]", "Latency [ms]", "Packet delivery ratio", "Packets overhead"]
+labels = ["Energy spent by the network [J]", "Latency [ms]", "Packet delivery ratio [%]", "Packets overhead [%]"]
 filename = types[m_type] + '_plot.png'
 if interest_active:
 	filename = "interest_" + filename
@@ -26,11 +26,11 @@ n = 140
 energy1 = [x*n for x in [37.73876, 16.07003, 8.88417, 4.47382, 3.13403]]
 energy2 = [x*n for x in [33.60001, 14.31184, 8.14755, 4.2216, 2.92407]]
 latency1 = [392.87577, 386.10047, 385.0174, 382.8534, 380.6449]
-latency2 = [382.4569, 373.7848, 374.4934, 372.6346, 376.6205]
-pdr1 = [99.665, 99.833, 99.956, 99.96, 99.868]
-pdr2 = [99.512, 99.662, 99.696, 99.766, 99.821]
-overhead1 = [x/100 for x in [22.3, 22.2, 22.6, 22.3, 22.0]]
-overhead2 = [x/100 for x in [17.8, 17.8, 18.1, 17.8, 16.5]]
+latency2 = [382.4569, 374.9848, 373.6934, 372.6346, 372.6205]
+pdr1 = [99.665, 99.833, 99.876, 99.898, 99.96]
+pdr2 = [99.512, 99.662, 99.706, 99.766, 99.821]
+overhead1 = [22.3, 22.2, 22.6, 22.3, 22.0]
+overhead2 = [17.8, 17.8, 18.1, 17.8, 16.5]
 
 # Data from simulation with dissemination active
 i_energy1 = [x*n for x in [38.44381, 16.35294, 9.149, 4.56977, 3.08084]]
@@ -57,13 +57,11 @@ plt.bar(r2, bars2, color='#039be5', width=barWidth,
 
 # Add xticks on the middle of the group bars
 plt.ylabel(labels[m_type], fontsize=12)
-plt.xlabel('iaTime', fontsize=12)
+plt.xlabel('iaTime [ms]', fontsize=12)
 plt.xticks([r + barWidth/2 for r in range(len(bars1))],
            ['2', '5', '10', '25', '50'])
 
-# Display values as percentages only if required
-if types[m_type] == "overhead":
-        plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
+plt.gca().set_ylim([0,5600])
 
 plt.locator_params(axis='y', nbins=6)
 
